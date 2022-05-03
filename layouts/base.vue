8714fca3e33a7a5b01963ae9cb497744b495b8ee
kurikuto <k.ab.kurikuto@gmail.com>
2022-05-03 12:40:28 +0900

    init app

---
+++ layouts/base.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+		<v-navigation-drawer app>
+			Navigation Drawer
+		</v-navigation-drawer>
+		<v-app-bar color="lime lighten-4" dark app>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			<v-toolbar-title>Vuetify</v-toolbar-title>
+		</v-app-bar>
+		<v-main>
+			<slot /> 
+		</v-main>
+		<v-footer color="lime lighten-4" dark app>
+			Vuetify
+		</v-footer>
+	</v-app>
+</template>
+
+<style scoped>
+	/* .v-app-bar {
+		width: 100%;
+	} */
+</style>
